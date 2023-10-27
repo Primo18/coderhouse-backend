4382b120f1522482c502efe6c8e4e856e228e35e
Daniel López <danilobo.elo@gmail.com>
2023-10-27 19:32:53 -0300

    add clase 19

---
+++ ejercicios/clase_19/src/app.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import FileStore from 'session-file-store';
+import MongoStore from 'connect-mongo';
+
+const app = express();
+const PORT = 3000;
+
+
+const fileStorage = FileStore(session);
+
+app.use(cookieParser());
+
+// app.use(session({
+//     secret: 'secret',
+//     resave: false,
+//     saveUninitialized: true,
+//     store: new fileStorage({ path: './sessions', ttl: 100, retries: 0 }),
+//     cookie: { maxAge: 1000 * 60 * 60 * 24 * 7 },
+// }));
+
+// MongoStore
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: 'mongodb+srv://primo:Rust.1830@devcluster.9xzyesc.mongodb.net/?retryWrites=true&w=majority' }),
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 7 },
+    retries: 0,
+}));
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
